610d68c5d788e52084afae9a0f3e2933f4d4f7f3
Lalo <76499838+laloquidity@users.noreply.github.com>
2024-03-03 22:57:15 -0500

    Update start-onomyd.sh with correct path

--- testnet/scripts/start-onomyd.sh
+++ testnet/scripts/start-onomyd.sh
@@ -3,9 +3,20 @@ set -eu
 
 echo "Starting onomy node"
 
+# Ensure the open file limit is sufficient
 if [ "$(ulimit -n)" -lt 65535 ]; then
     echo "Fail ulimit: $(ulimit -n) < 65535"
     exit 1
 fi
 
-onomyd start
+# Dynamically construct the path to the onomyd executable
+ONOMYD_PATH="$HOME/.onomy/cosmovisor/genesis/bin/onomyd"
+
+# Check if the onomyd executable exists at the constructed path
+if [ ! -f "$ONOMYD_PATH" ]; then
+    echo "onomyd executable not found at $ONOMYD_PATH."
+    exit 1
+fi
+
+# Start the onomyd using the dynamically found path
+$ONOMYD_PATH start
